5cc622f4bfad87b637b749629d434bc305496c6a
Roland Arsenault <roland@ccom.unh.edu>
2018-04-06 13:27:10 -0400

    More info on AX8 resource tree.

--- src/flir/flir.py
+++ src/flir/flir.py
@@ -23,6 +23,45 @@ wget --post-data 'action=get&resource=.image.services.store.filename' http://192
 .image.sysimg.fusion.fusionData.fusionMode 1
 .image.sysimg.fusion.fusionData.useLevelSpan 0
 
+
+
+The variable that controls the Lamp is .system.vcam.torch
+
+
+
+In order to enable or disable a given alarm the following Boolean registers should be changed. These registers are accessible via Pass Through Object (EtherNet/IP).
+
+.resmon.items.<alarm#>.active
+
+.image.sysimg.alarms.measfunc.<alarm#>.active
+
+
+Enabling alarm 1
+
+.resmon.items.1.active TRUE
+
+16 2e 72 65 73 6d 6f 6e 2e 69 74 65 6d 73 2e 31 2e 61 63 74 69 76 65 01
+
+.image.sysimg.alarms.measfunc.1.active TRUE
+
+26 2e 69 6d 61 67 65 2e 73 79 73 69 6d 67 2e 61 6c 61 72 6d 73 2e 6d 65 61 73 66 75 6e 63 2e 31 2e 61 63 74 69 76 65 01
+
+ 
+
+Disabling alarm 3
+
+.resmon.items.3.active FALSE
+
+16 2e 72 65 73 6d 6f 6e 2e 69 74 65 6d 73 2e 33 2e 61 63 74 69 76 65 00
+
+.image.sysimg.alarms.measfunc.3.active FALSE
+
+26 2e 69 6d 61 67 65 2e 73 79 73 69 6d 67 2e 61 6c 61 72 6d 73 2e 6d 65 61 73 66 75 6e 63 2e 33 2e 61 63 74 69 76 65 00
+
+
+See also: BasicICD.pdf
+
+
 ''' 
 
 
