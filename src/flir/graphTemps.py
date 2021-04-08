c80deb07a94a2d0c92880e3fe8776c8be27e8286
Roland Arsenault <roland@ccom.unh.edu>
2021-04-08 09:33:13 -0400

    Fix for python3

--- src/flir/graphTemps.py
+++ src/flir/graphTemps.py
@@ -12,7 +12,7 @@ plt.ion()
 
 while True:
     b = f.getBox(1)
-    print b['minT'],b['avgT'],b['maxT']
+    print (b['minT'],b['avgT'],b['maxT'])
     data['t'].append(datetime.datetime.utcnow())
     data['min'].append(float(b['minT'].strip('"')[:-1]))
     data['avg'].append(float(b['avgT'].strip('"')[:-1]))
